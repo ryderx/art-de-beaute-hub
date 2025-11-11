37b5220a87639b75bbcf69a7ca88eac6441e231e
Luis Custodia <mambocinco@gmail.com>
2025-11-11 11:51:16 -0400

    contact section edited

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -43,6 +43,7 @@ const ContactSection = () => {
                 <div>
                   <h4 className="font-semibold mb-1">{t('contact.phone')}</h4>
                   <p className="text-gray-600">07.82.22.05.63</p>
+                  <p className="text-gray-600">09.52.36.79.21</p>
                 </div>
               </div>
               
@@ -52,7 +53,7 @@ const ContactSection = () => {
                 </div>
                 <div>
                   <h4 className="font-semibold mb-1">{t('contact.email')}</h4>
-                  <p className="text-gray-600">munozjulia1977@gmail.com</p>
+                  <p className="text-gray-600">artetcosmetique@gmail.com</p>
                 </div>
               </div>
               
@@ -79,14 +80,14 @@ const ContactSection = () => {
               <h4 className="font-semibold mb-3">{t('contact.followUs')}</h4>
               <div className="flex space-x-4">
                 <a 
-                  href="#" 
+                  href="https://www.facebook.com/profile.php?id=100050440226828" 
                   className="bg-beauty-pink hover:bg-beauty-darkpink text-beauty-darkpink hover:text-white p-2 rounded-full transition-colors"
                   aria-label="Facebook"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
                 </a>
                 <a 
-                  href="#" 
+                  href="https://www.instagram.com/artdelabeautee?igsh=aGtqZnVjaGZncDEz" 
                   className="bg-beauty-pink hover:bg-beauty-darkpink text-beauty-darkpink hover:text-white p-2 rounded-full transition-colors"
                   aria-label="Instagram"
                 >
